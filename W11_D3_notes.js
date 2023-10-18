bc04cdf3da0eb245bcbdbf8a629e1e640fd04b5a
Anthony Lovern <voltorodarkwatch@gmail.com>
2023-10-18 12:34:57 -0400

    Through Sequelize READ lecture

---
+++ W11_D3_notes.js
@@ -0,0 +1,30 @@
+/*
+
+There are a variety of query methods that are built into our models
+    findAll
+        Useful when we want multiple records
+        Always returns an array
+    findOne
+        Useful when we want a single record
+        If we target a column that isn't UNIQUE, it will add a LIMIT 1 to the SQL
+    findByPk
+        Also when finding just a single record
+        Basically a findOne, but shortcuts to searching via Primary Key
+        First arg is the PK, not the options object
+    All of these methods can take in an object
+    To add a where clause, we add the where property which points to an object:
+        {
+            <col name>: <value>
+        }
+    
+    ORDER BY
+        order property
+            Points to a 2D array [[<col name>, <'ASC'/'DESC'>]]
+
+    LIKE
+        We can use a Sequelize Operator
+        https://sequelize.org/docs/v6/core-concepts/model-querying-basics/#operators
+
+
+
+*/
